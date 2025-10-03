edf90c24b49b25bbc5fae82b11b247ca41953da3
Vitaliy Krukov <halva1236@gmail.com>
2025-10-04 00:36:24 +0300

    домашка 30.2 переработка задания

--- users/views.py
+++ users/views.py
@@ -1,33 +1,21 @@
 from rest_framework import viewsets
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from .models import Payment
 from .serializers import PaymentSerializer
+from .filters import PaymentFilter
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
-    """ViewSet для платежей с простой фильтрацией"""
 
+
+    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
-    def get_queryset(self):
-        queryset = Payment.objects.all()
 
-        course_id = self.request.query_params.get("course")
-        if course_id:
-            queryset = queryset.filter(paid_course_id=course_id)
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = PaymentFilter
 
-        lesson_id = self.request.query_params.get("lesson")
-        if lesson_id:
-            queryset = queryset.filter(paid_lesson_id=lesson_id)
 
-        payment_method = self.request.query_params.get("method")
-        if payment_method in ["cash", "transfer"]:
-            queryset = queryset.filter(payment_method=payment_method)
-
-        ordering = self.request.query_params.get("ordering", "desc")
-        if ordering == "asc":
-            queryset = queryset.order_by("payment_date")
-        else:
-            queryset = queryset.order_by("-payment_date")
-
-        return queryset
+    ordering_fields = ['payment_date', 'amount']
+    ordering = ['-payment_date']  # Сортировка по умолчанию
